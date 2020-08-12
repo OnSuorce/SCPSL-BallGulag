bc65d52cae01772a7d07ed2e51fccd499292d8f0
OnSuorce <bianchi.57.matteo@gmail.com>
2020-08-12 13:45:42 +0200

    First Commit

---
+++ BallGulag/BallGulag/BallGulag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Exiled.API.Features;
+
+
+namespace BallGulag
+{
+    class BallGulag : Plugin<Config>
+    {
+        private static readonly Lazy<BallGulag> LazyInstance = new Lazy<BallGulag>(() => new BallGulag());
+        public static BallGulag pluginInstance => LazyInstance.Value;
+        private Handlers.GulagEvent GulagEvent;
+
+        public override void OnEnabled()
+        {
+
+            Register();
+        }
+        public override void OnDisabled()
+        {
+            UnRegister();
+        }
+        public void Register()
+        {
+            GulagEvent = new Handlers.GulagEvent();
+            //Exiled.Events.Handlers.Scp914.UpgradingItems += GulagEvent;
+            //Exiled.Events.Handlers.Player.Died += GulagEvent;
+
+        }
+        public void UnRegister()
+        {
+            //Exiled.Events.Handlers.Scp914.UpgradingItems -= GulagEvent.OnUpgrading;
+            //Exiled.Events.Handlers.Player.Died -= GulagEvent.OnPlayerDead;
+
+            GulagEvent = null;
+        }
+    }
+}
---
+++ BallGulag/BallGulag/Config.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+namespace BallGulag
+{
+    class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+    }
+}
